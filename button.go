dc7f706c7d3019de820d3e31a6717b68d17e2074
lsafd <65264005+lsafd@users.noreply.github.com>
2021-02-10 19:34:13 +0000

    Update button.go

--- button.go
+++ button.go
@@ -4,10 +4,10 @@ import (
 	"image"
 	"image/color"
 
-	"fyne.io/fyne"
-	"fyne.io/fyne/canvas"
-	"fyne.io/fyne/theme"
-	"fyne.io/fyne/widget"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 	"github.com/unix-streamdeck/api"
 )
 
