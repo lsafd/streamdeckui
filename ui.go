1598428e5216a9a3593c35e7766abfee5d3cd5ab
lsafd <65264005+lsafd@users.noreply.github.com>
2021-02-10 19:32:30 +0000

    Update ui.go

--- ui.go
+++ ui.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"fyne.io/fyne"
-	"fyne.io/fyne/dialog"
-	"fyne.io/fyne/layout"
-	"fyne.io/fyne/theme"
-	"fyne.io/fyne/widget"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 	"github.com/unix-streamdeck/api"
 )
 
